bf0d99707d785d4fc09f2b792198b4f8493243fc
daniel <hellodanielbai@gmail.com>
2022-01-21 17:31:50 -0800

    gitignore & applicationjs update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require_jquery
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+
