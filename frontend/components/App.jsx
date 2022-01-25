d122f4e3b98d0a49de7e35ca16c52a46bbff87ee
daniel <hellodanielbai@gmail.com>
2022-01-24 19:01:10 -0800

    debugging more

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,8 +3,8 @@ import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 
 import GreetingContainer from './greeting/greeting_container';
-import SignUpFormContainer from './session_form/signup_form_container';
-import LogInFormContainer from './session_form/login_form_container';
+// import SignUpFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 
@@ -12,11 +12,8 @@ const App = () => (
     <div>
         <header>
             <h1>Jillow</h1>
-
         </header>
-
         <Route path="/login" component={LoginFormContainer} />
-        <Route path="/signup" component={SignupFormContainer} />
     </div>
 );
 
