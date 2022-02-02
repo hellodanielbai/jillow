7a1b58727a395a4c5dd0806f0d0f861d1d5682fd
daniel <hellodanielbai@gmail.com>
2022-02-01 22:26:08 -0800

    started map

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,10 +1,15 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions'
+import { RECEIVE_LISTING } from '../actions/listings_actions'
 
 const usersReducer = (state = {}, action) => {
     Object.freeze(state)
     switch(action.type) {
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, state, { [action.currentUser.id]: action.currentUser })
+        // case RECEIVE_SAVE:
+        //     return Object.assign({}, state, { [action.lister.id]: action.lister });
+        // case RECEIVE_LISTING:
+        //     return Object.assign({}, state, action.listers);
         default:
             return state;
     }
